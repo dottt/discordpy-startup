01964a523ebec8d8182082a8eaca1474ed7cc834
dottt <dot0609@gmail.com>
2020-08-26 02:18:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -27,10 +27,11 @@ async def rect(ctx, about = "募集", cnt = 4, settime = 10.0):
     def check(reaction, user):
         emoji = str(reaction.emoji)
         if user.bot == True:    # botは無視
+            print('user.bot');
             pass
         else:
             return emoji == '⏫' or emoji == '✖'
-    print( len(reaction_member));
+
     while len(reaction_member)-1 <= cnt:
         try:
             reaction, user = await client.wait_for('reaction_add', timeout=settime, check=check)
